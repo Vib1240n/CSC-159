e2a1def3468bc79690d2719e9f7cff64974106c2
vib1240n <vibhorsgr@gmail.com>
2022-10-09 02:11:56 +0000

    tty testing

--- src/tty.c
+++ src/tty.c
@@ -40,6 +40,9 @@ void tty_refresh(void) {
     if (!active_tty) {
         kernel_panic("No TTY is selected!");
         return;
+    }else {
+        vga_putc_at(active_tty->pos_x, active_tty->pos_y, active_tty->color_bg, active_tty->color_fg, c);
+        active_tty->refresh = 0;
     }
 
     // If the TTY needs to be refreshed, copy the tty buffer
@@ -56,7 +59,73 @@ void tty_update(char c) {
     if (!active_tty) {
         return;
     }
+    //  unsigned short *vga_base = (unsigned short*)(0xB8000);
 
+    // // Handling backspace
+    // if(c == 0x08){
+    //     c = 0x00; 
+    //     *(unsigned short *)(vga_base + vga_pos_y * 80 + vga_pos_x) = (unsigned short)VGA_CHAR(vga_color_bg, vga_color_fg, c);
+    //     vga_pos_x -= 1;
+        
+    //     if(vga_pos_x < 0 && vga_pos_y > 0){
+    //         vga_pos_x = VGA_WIDTH - 1;
+    //         vga_pos_y -= 1;
+    //     }
+    //     else if(vga_pos_x < 0 && vga_pos_y < 0){
+    //         vga_pos_x = 0;
+    //         vga_pos_y = 0;
+    //     }
+    // }
+    // // For Tab
+    // else if(c == 0x09){
+    //     c = 0x20; 
+    //     for(int j=0; j<4; j++){
+    //         *(unsigned short *)(vga_base + vga_pos_y * 80 + vga_pos_x) = (unsigned short)VGA_CHAR(vga_color_bg, vga_color_fg, c);
+    //         vga_pos_x += 1;
+
+    //         if(vga_pos_x < 0 && vga_pos_y > 0){
+    //             vga_pos_x = VGA_WIDTH - 1;
+    //             vga_pos_y -= 1;
+    //         }
+    //         else if(vga_pos_x < 0 && vga_pos_y < 0){
+    //             vga_pos_x = 0;
+    //             vga_pos_y = 0;
+    //         }
+    //     }
+
+    // }
+    // // For Carriage Return
+    // else if(c == 0x0D){
+    //     vga_pos_x = 0;
+    // }
+    // // For New Line
+    // else if(c == 0x0A){
+    //     vga_pos_x = 0;
+    //     vga_pos_y += 1;
+    //     if(vga_pos_y > VGA_HEIGHT-2){
+    //         for(int i=0; i< VGA_HEIGHT-1; i++){
+    //             for(int j=0; j< VGA_WIDTH; j++){
+    //                 *(unsigned short *)(vga_base + i * 80 + j)= *(unsigned short*)(vga_base + (i+1) * 80 + j);
+    //             }
+    //         }
+    //         vga_pos_y = VGA_HEIGHT-1;
+    //         for(int h=0; h< VGA_WIDTH-1; h++){
+    //             *(unsigned short *)(vga_base + vga_pos_y * 80 + h) = (unsigned short )VGA_CHAR(vga_color_bg, vga_color_fg, 0x00);
+    //         }
+                    
+
+    //     }
+    // }
+    // else{ 
+    //     *(unsigned short *)(vga_base + vga_pos_y * 80 + vga_pos_x) = (unsigned short)VGA_CHAR(vga_color_bg, vga_color_fg, c);
+    //     vga_pos_x += 1;
+    // }
+    
+
+    // if(vga_pos_x > VGA_WIDTH - 1){
+    //     vga_pos_x = 0;
+    //     vga_pos_y += 1;
+    // }
     // Since this is a virtual wrapper around the VGA display, treat each
     // input character as you would for the VGA output
     //   Adjust the x/y positions as necessary
@@ -77,15 +146,23 @@ void tty_update(char c) {
 void tty_init(void) {
     kernel_log_info("tty: Initializing TTY driver");
 
-    //this.x = 0;
-    //this.y = 0;
-    //this.refresh = 0;
-
     // Initialize the tty_table
+    for(int i = 0; i< TTY_MAX; i++){
+        tty_table[i].id = 0;
+        tty_table[i].pos_x = 0;
+        tty_table[i].pos_y = 0;
+        tty_table[i].pos_scroll = 0;
+        tty_table[i].color_bg = VGA_COLOR_BLACK;
+        tty_table[i].color_fg = VGA_COLOR_WHITE;
+        tty_table[i].refresh = 0;
+        tty_table[i].buf[i] = 0;
+    }
     
 
     // Select tty 0 to start with
+    tty_select(0);
 
     // Register a timer callback to update the screen on a regular interval
+    timer_callback_register(tty_refresh(), 100, -1);
 }
 
