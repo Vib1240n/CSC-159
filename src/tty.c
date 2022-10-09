710563ca8186bb102c838c0bc04e832aefff543e
vib1240n <vibhorsgr@gmail.com>
2022-10-09 02:43:43 +0000

    tty testing 4

--- src/tty.c
+++ src/tty.c
@@ -19,6 +19,8 @@ struct tty_t tty_table[TTY_MAX];
 // Current Active TTY
 struct tty_t *active_tty;
 
+void tty_refersh(void);
+
 /**
  * Sets the active TTY to the selected TTY number
  * @param tty - TTY number
@@ -60,77 +62,12 @@ void tty_update(char c) {
     if (!active_tty) {
         return;
     }
-    //  unsigned short *vga_base = (unsigned short*)(0xB8000);
 
-    // // Handling backspace
-    // if(c == 0x08){
-    //     c = 0x00; 
-    //     *(unsigned short *)(vga_base + vga_pos_y * 80 + vga_pos_x) = (unsigned short)VGA_CHAR(vga_color_bg, vga_color_fg, c);
-    //     vga_pos_x -= 1;
-        
-    //     if(vga_pos_x < 0 && vga_pos_y > 0){
-    //         vga_pos_x = VGA_WIDTH - 1;
-    //         vga_pos_y -= 1;
-    //     }
-    //     else if(vga_pos_x < 0 && vga_pos_y < 0){
-    //         vga_pos_x = 0;
-    //         vga_pos_y = 0;
-    //     }
-    // }
-    // // For Tab
-    // else if(c == 0x09){
-    //     c = 0x20; 
-    //     for(int j=0; j<4; j++){
-    //         *(unsigned short *)(vga_base + vga_pos_y * 80 + vga_pos_x) = (unsigned short)VGA_CHAR(vga_color_bg, vga_color_fg, c);
-    //         vga_pos_x += 1;
-
-    //         if(vga_pos_x < 0 && vga_pos_y > 0){
-    //             vga_pos_x = VGA_WIDTH - 1;
-    //             vga_pos_y -= 1;
-    //         }
-    //         else if(vga_pos_x < 0 && vga_pos_y < 0){
-    //             vga_pos_x = 0;
-    //             vga_pos_y = 0;
-    //         }
-    //     }
-
-    // }
-    // // For Carriage Return
-    // else if(c == 0x0D){
-    //     vga_pos_x = 0;
-    // }
-    // // For New Line
-    // else if(c == 0x0A){
-    //     vga_pos_x = 0;
-    //     vga_pos_y += 1;
-    //     if(vga_pos_y > VGA_HEIGHT-2){
-    //         for(int i=0; i< VGA_HEIGHT-1; i++){
-    //             for(int j=0; j< VGA_WIDTH; j++){
-    //                 *(unsigned short *)(vga_base + i * 80 + j)= *(unsigned short*)(vga_base + (i+1) * 80 + j);
-    //             }
-    //         }
-    //         vga_pos_y = VGA_HEIGHT-1;
-    //         for(int h=0; h< VGA_WIDTH-1; h++){
-    //             *(unsigned short *)(vga_base + vga_pos_y * 80 + h) = (unsigned short )VGA_CHAR(vga_color_bg, vga_color_fg, 0x00);
-    //         }
-                    
-
-    //     }
-    // }
-    // else{ 
-    //     *(unsigned short *)(vga_base + vga_pos_y * 80 + vga_pos_x) = (unsigned short)VGA_CHAR(vga_color_bg, vga_color_fg, c);
-    //     vga_pos_x += 1;
-    // }
-    
-
-    // if(vga_pos_x > VGA_WIDTH - 1){
-    //     vga_pos_x = 0;
-    //     vga_pos_y += 1;
-    // }
     // Since this is a virtual wrapper around the VGA display, treat each
     // input character as you would for the VGA output
     //   Adjust the x/y positions as necessary
     //   Handle scrolling at the bottom
+    active_tty->buf[] = (unsigned short)VGA_CHAR(active_tty->color_bg, active_tty->color_fg, c);
 
     // Instead of writing to the VGA display directly, you will write
     // to the tty buffer.
