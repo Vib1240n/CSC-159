cba28a056571bd14e7571395dba6b6de14232e1e
Christian Suarez <94643832+skyyear@users.noreply.github.com>
2022-10-21 17:50:08 -0700

    adding more to vga

--- src/kproc.c
+++ src/kproc.c
@@ -180,6 +180,7 @@ void kproc_init(void) {
         table->start_time = 0;
         table->run_time = 0;
         table->cpu_time = 0;
+        table->stack = &proc_stack[PROC_MAX][PROC_STACK_SIZE];
     }
 
 
@@ -195,11 +196,15 @@ void kproc_init(void) {
     //   - process stack
 
 
-    
+
+
+
 
 
     // Create the idle process (kproc_idle) as a kernel process
 
+    
+    kproc_idle();
 
 
 }
